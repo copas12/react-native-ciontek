ad125bf1294d51ab596bec9c286f7af88cc4684e
imronimamwg <imronimam11@gmail.com>
2019-12-05 17:35:42 +0700

    add emvcoHelper

--- android/src/main/java/com/reactlibrary/CiontekPrinterModule.java
+++ android/src/main/java/com/reactlibrary/CiontekPrinterModule.java
@@ -5,16 +5,33 @@ import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
 import com.facebook.react.bridge.Callback;
 
+import com.reactlibrary.service.MyService;
 import vpos.apipackage.PosApiHelper;
 import vpos.apipackage.Print;
 import vpos.apipackage.PrintInitException;
+import vpos.keypad.EMVCOHelper;
 
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.util.Log;
 import android.os.Handler;
-import android.widget.Toast;  
+import android.widget.Toast;
 
+import android.content.BroadcastReceiver;
+import android.content.ComponentName;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.ServiceConnection;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.IBinder;
+import android.os.Message;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class CiontekPrinterModule extends ReactContextBaseJavaModule {
 
@@ -27,12 +44,17 @@ public class CiontekPrinterModule extends ReactContextBaseJavaModule {
     private int RESULT_CODE = 0;
     private int cycle_num = 0;
 
+    private int BatteryV;
+    private int voltage_level;
+
     final int PRINT_TEST = 0;
     final int PRINT_UNICODE = 1;
 
     int ret = -1;
 
     PosApiHelper posApiHelper = PosApiHelper.getInstance();
+    Print print = new Print();
+    EMVCOHelper emvcoHelper = EMVCOHelper.getInstance();
 
     public CiontekPrinterModule(ReactApplicationContext reactContext) {
         super(reactContext);
@@ -50,62 +72,138 @@ public class CiontekPrinterModule extends ReactContextBaseJavaModule {
         callback.invoke("Received numberArgument: " + numberArgument + " stringArgument: " + stringArgument);
     }
 
-    private void setValue(int val) {
-        sp = mContext.getSharedPreferences("Gray", mContext.MODE_PRIVATE);
-        SharedPreferences.Editor editor = sp.edit();
-        editor.putInt("value", val);
-        editor.commit();
+    public class BatteryReceiver extends BroadcastReceiver {
+        public void onReceive(Context context, Intent intent) {
+            voltage_level = intent.getExtras().getInt("level");
+            BatteryV = intent.getIntExtra("voltage", 0);
+        }
     }
 
-    private int getValue() {
-        sp = mContext.getSharedPreferences("Gray", mContext.MODE_PRIVATE);
-        int value = sp.getInt("value", 2);
-        return value;
-    }
+    // private static final String DISABLE_FUNCTION_LAUNCH_ACTION = "android.intent.action.DISABLE_FUNCTION_LAUNCH";
+    // private void disableFunctionLaunch(boolean state) {
+    //     Intent disablePowerKeyIntent = new Intent(DISABLE_FUNCTION_LAUNCH_ACTION);
+    //     if (state) {
+    //         disablePowerKeyIntent.putExtra("state", true);
+    //     } else {
+    //         disablePowerKeyIntent.putExtra("state", false);
+    //     }
+    //     sendBroadcast(disablePowerKeyIntent);
+    // }
+
+    ServiceConnection serviceConnection = new ServiceConnection() {
+        @Override
+        public void onServiceConnected(ComponentName name, IBinder service) {
+            MyService.MyBinder binder = (MyService.MyBinder) service;
+            MyService myService = binder.getService();
+
+            myService.setCallback(new MyService.CallBackPrintStatus() {
+                @Override
+                public void printStatusChange(String strStatus) {
+                    // SendMsg(strStatus);
+                }
+            });
+
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName name) {
+
+        }
+    };
 
     @ReactMethod
     public void testPrint() {
-        Thread myThread = new Thread() {
+        // PosApiHelper.getInstance().SysLogSwitch(1);
+        int emv = emvcoHelper.AdapterUartBaud();
+        Log.v("Ciontek", "emv init: " + emv);
+
+        new Thread(new Runnable() {
+            @Override
             public void run() {
+                posApiHelper.SysLogSwitch(1);
                 Log.v("Ciontek", "Prepare ...");
-               
-                ret = posApiHelper.PrintInit(2, 24, 24, 0);
-                // checkRet(ret, "ret PrintInit");
-                
+
+                try {
+                    ret = posApiHelper.PrintInit();
+                } catch (PrintInitException e) {
+                    e.printStackTrace();
+                }
                 Log.v("Ciontek", "ret init: " + ret);
-                ret = posApiHelper.PrintSetFont((byte) 24, (byte) 24, (byte) 0x00);
-                // checkRet(ret, "ret PrintSetFont");
+
+                posApiHelper.PrintSetGray(1);
+
+                ret = posApiHelper.PrintCheckStatus();
+                if (ret == -1) {
+                    RESULT_CODE = -1;
+                    return;
+                } else if (ret == -2) {
+                    RESULT_CODE = -1;
+                    return;
+                } else if (ret == -3) {
+                    RESULT_CODE = -1;
+                    return;
+                } else {
+                    RESULT_CODE = 0;
+                }
+                Log.v("Ciontek", "ret check status: " + ret + ", result_code: " + RESULT_CODE);
+
+                posApiHelper.PrintSetFont((byte) 24, (byte) 24, (byte) 0x00);
                 Log.v("Ciontek", "ret set font: " + ret);
-                    
-        
+
                 posApiHelper.PrintStr("中文:你好，好久不见。\n");
                 posApiHelper.PrintStr("英语:Hello, Long time no see   ￡ ：2089.22\n");
                 posApiHelper.PrintStr("意大利语Italian :Ciao, non CI vediamo da Molto Tempo.\n");
                 posApiHelper.PrintStr("西班牙语:España, ¡Hola! Cuánto tiempo sin verte!\n");
-                posApiHelper.PrintStr("Arabic:");
-                posApiHelper.PrintStr("قل مرحبا عند مقابلتك");
-                posApiHelper.PrintStr("الفبای فارسی گروه سی‌ودوگانهٔ");
+                posApiHelper.PrintStr("Arabic:");// 阿拉伯语
+                posApiHelper.PrintStr("قل مرحبا عند مقابلتك");// 阿拉伯语
+                posApiHelper.PrintStr("الفبای فارسی گروه سی‌ودوگانهٔ");// 阿拉伯语
                 posApiHelper.PrintStr("سی‌ودوگانهٔ");
                 posApiHelper.PrintStr("حروف الفبا یا حروف هجای فارسی می‌گویند");
-                posApiHelper.PrintStr("الفبای فارسی گروه سی‌ودوگانهٔ حروف (اَشکال نوشتاری) در خط فارسی است که نمایندهٔ نگاشتن (همخوان‌ها یا صامت‌ها) در زبان فارسی است و");
-                posApiHelper.PrintStr("است که نمایندهٔ نگاشتن (همخوان‌ها یا صامت‌ها) در زبان فارسی است و");
-                posApiHelper.PrintStr("泰语:สวัสดีครับไม่เจอกันนานเลยนะ!\n");
-                posApiHelper.PrintStr("法语:Bonjour! Ça fait longtemps!\n");
-                posApiHelper.PrintStr("                                         \n");
-                posApiHelper.PrintStr("                                         \n");
-        
-                
+
                 ret = posApiHelper.PrintStart();
-                // checkRet(ret, "ret PrintStart");
+                // ret = print.Lib_PrnStart();
                 Log.v("Ciontek", "ret print start: " + ret);
-
-                   
-
             }
-       
-        };
-        myThread.start();
-     
+        }).start();
+        // Thread myThread = new Thread() {
+        // public void run() {
+        // Log.v("Ciontek", "Prepare ...");
+
+        // ret = posApiHelper.PrintInit(2, 24, 24, 0);
+        // // checkRet(ret, "ret PrintInit");
+
+        // Log.v("Ciontek", "ret init: " + ret);
+        // ret = posApiHelper.PrintSetFont((byte) 24, (byte) 24, (byte) 0x00);
+        // // checkRet(ret, "ret PrintSetFont");
+        // Log.v("Ciontek", "ret set font: " + ret);
+
+        // posApiHelper.PrintStr("中文:你好，好久不见。\n");
+        // posApiHelper.PrintStr("英语:Hello, Long time no see ￡ ：2089.22\n");
+        // posApiHelper.PrintStr("意大利语Italian :Ciao, non CI vediamo da Molto Tempo.\n");
+        // posApiHelper.PrintStr("西班牙语:España, ¡Hola! Cuánto tiempo sin verte!\n");
+        // posApiHelper.PrintStr("Arabic:");
+        // posApiHelper.PrintStr("قل مرحبا عند مقابلتك");
+        // posApiHelper.PrintStr("الفبای فارسی گروه سی‌ودوگانهٔ");
+        // posApiHelper.PrintStr("سی‌ودوگانهٔ");
+        // posApiHelper.PrintStr("حروف الفبا یا حروف هجای فارسی می‌گویند");
+        // posApiHelper.PrintStr("الفبای فارسی گروه سی‌ودوگانهٔ حروف (اَشکال نوشتاری) در
+        // خط فارسی است که نمایندهٔ نگاشتن (همخوان‌ها یا صامت‌ها) در زبان فارسی است و");
+        // posApiHelper.PrintStr("است که نمایندهٔ نگاشتن (همخوان‌ها یا صامت‌ها) در زبان
+        // فارسی است و");
+        // posApiHelper.PrintStr("泰语:สวัสดีครับไม่เจอกันนานเลยนะ!\n");
+        // posApiHelper.PrintStr("法语:Bonjour! Ça fait longtemps!\n");
+        // posApiHelper.PrintStr(" \n");
+        // posApiHelper.PrintStr(" \n");
+
+        // ret = posApiHelper.PrintStart();
+        // // checkRet(ret, "ret PrintStart");
+        // Log.v("Ciontek", "ret print start: " + ret);
+
+        // }
+
+        // };
+        // myThread.start();
+
     }
 
     @ReactMethod
@@ -116,11 +214,8 @@ public class CiontekPrinterModule extends ReactContextBaseJavaModule {
 
     private void checkRet(int ret, String message) {
         if (ret < 0) {
-            Toast.makeText(mContext, message + " : " + ret,Toast.LENGTH_SHORT).show();  
+            Toast.makeText(mContext, message + " : " + ret, Toast.LENGTH_SHORT).show();
         }
     }
- 
-    
-    
-}
 
+}
